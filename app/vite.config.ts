f3d98ce31b2184aaa24b542c13bd6671111d41af
Christian Herrero <christian.herrero@canda.com>
2026-05-25 12:40:26 +0200

    Fix dist path

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     root: __dirname,
     plugins: [react()],
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
 });
